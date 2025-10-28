1a1a46a3cbb42dc3825eeeb7bd486050ef8944ff
Tempest Source <tempestsource@gmail.com>
2025-10-27 22:11:38 -0500

    virtmanager

--- modules/core/dev/vm.nix
+++ modules/core/dev/vm.nix
@@ -1,3 +1,7 @@
 { userName, ... }: {
-  virtualisation.virtualbox.host.enable = true;
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["${userName}"];
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 }
